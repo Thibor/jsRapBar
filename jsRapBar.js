eba46de36d3ff2a2f8a8ca4c89541561b70209b0
Thibor <thibor@vp.pl>
2018-08-23 04:09:27 +0100

    Add files via upload

--- jsRapBar.js
+++ jsRapBar.js
@@ -1,21 +1,18 @@
 (function($){
 $.fn.jsRapBar = function(options){
 var defaults = {
-			enabled:true,
-			position:0,
-			width:'100%',
-			height:'16px',
-			barColor:'green',
-			backColor:'white'
+	enabled:true,
+	position:0,
+	width:'100%',
+	height:'16px',
+	barColor:'green',
+	backColor:'white'
 }
 	
 return this.each(function() {
 	this.opt = $.extend(defaults,options);
 	$(this).css({width:this.opt.width,height:this.opt.height,background:this.opt.backColor}).addClass('rapBarOut');
-	var inBar=$('<div>',{
-			class:'rapBarIn',
-			css:{'background-color':this.opt.barColor,'width':this.opt.position * 100 + '%'}
-	}).appendTo($(this));
+	var inBar = $('<div>').addClass('rapBarIn').css({'background-color':this.opt.barColor,'width':this.opt.position * 100 + '%'}).appendTo($(this));
 	if(this.opt.enabled)
 		$(this).bind({
 			click : function(e){
@@ -29,10 +26,10 @@ return this.each(function() {
 	
 	function findRealLeft(obj) {
 			  if( !obj ) return 0;
-			  return obj.offsetLeft + findRealLeft( obj.offsetParent );
+			  return obj.offsetLeft + findRealLeft(obj.offsetParent);
 			};
 			
-	this.SetPosition=function(p){
+	this.SetPosition = function(p){
 		this.opt.position = p;
 		inBar.css('width',p * 100 + '%');
 	}

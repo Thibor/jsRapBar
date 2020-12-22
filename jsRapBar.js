63f0d03886a72455637b75d4c0c8791fc71ee6cb
Thibor <thibor@vp.pl>
2020-12-22 07:37:16 +0000

    update

--- jsRapBar.js
+++ jsRapBar.js
@@ -1,60 +1,65 @@
-(function($){
-$.fn.jsRapBar = function(options){
-	
-return this.each(function(){
-	this.opt = $.extend({
-		enabled:true,
-		position:0,
-		width:'100%',
-		height:'16px',
-		barColor:'green',
-		backColor:'white',
-		onMouseUp:null,
-		onChange:null
-	},options);
-	let base = this;
-	let button = 0;
-	$(this).css({width:this.opt.width,height:this.opt.height,background:this.opt.backColor}).addClass('rapBarOut');
-	let inBar = $('<div>').addClass('rapBarIn').css({'background-color':this.opt.barColor}).appendTo($(this));
-	
-	if(this.opt.enabled)
-		$(this).bind({
-			click : function(e){
-				Update(e);
-			},
-			mousemove:function(e){
-				if(button)
-					Update(e)
-			},
-			mousedown:function(e){
-				button = 1;
-				e = e || window.event;
-				e.stopPropagation();
-				e.preventDefault();
-				if(this.setCapture)
-					this.setCapture();
-			},
-			mouseup:function(e){
-				button = 0;
-				if(this.opt.onMouseUp)
-					this.opt.onMouseUp.call(this,this.opt.position);
-			}
-		});	
-		
-	function Update(e){
-		let cw = e.clientX - $(base)[0].getBoundingClientRect().left;
-		let bw = $(base).width();
-		base.SetPosition(cw / bw);
-	}
-			
-	this.SetPosition = function(p){
-		this.opt.position = p;
-		inBar.css('width',p * 100 + '%');
-		if(this.opt.onChange)
-			this.opt.onChange.call(this,p);
-	}
-	
-	this.SetPosition(this.opt.position);	
-})
+(function ($) {
+	$.fn.jsRapBar = function (options) {
 
-}})(jQuery);
+		return this.each(function () {
+			this.opt = $.extend({
+				enabled: true,
+				position: 0,
+				width: '100%',
+				height: '16px',
+				barColor: 'green',
+				backColor: 'white',
+				onMouseUp: null,
+				onChange: null
+			}, options);
+			let base = this;
+			let button = 0;
+			$(this).css({ width: this.opt.width, height: this.opt.height, background: this.opt.backColor }).addClass('rapBarOut');
+			let inBar = $('<div>').addClass('rapBarIn').css({ 'background-color': this.opt.barColor }).appendTo($(this));
+
+			if (this.opt.enabled)
+				$(this).bind({
+					click: function (e) {
+						Update(e);
+					},
+					mousemove: function (e) {
+						if (button)
+							Update(e)
+					},
+					mousedown: function (e) {
+						button = 1;
+						e = e || window.event;
+						e.stopPropagation();
+						e.preventDefault();
+						if (this.setCapture)
+							this.setCapture();
+					},
+					mouseup: function (e) {
+						button = 0;
+						if (this.opt.onMouseUp)
+							this.opt.onMouseUp.call(this, this.opt.position);
+					}
+				});
+
+			function Update(e) {
+				let cw = e.clientX - $(base)[0].getBoundingClientRect().left;
+				let bw = $(base).width();
+				base.SetPosition(cw / bw);
+			}
+
+			this.SetPosition = function (p) {
+				if (p < 0)
+					p = 0;
+				if (p > 1)
+					p = 1;
+				this.opt.position = p;
+				inBar.css('width', p * 100 + '%');
+				if (this.opt.onChange)
+					this.opt.onChange.call(this, p);
+			}
+
+			this.SetPosition(this.opt.position);
+		})
+
+	}
+})(jQuery);
